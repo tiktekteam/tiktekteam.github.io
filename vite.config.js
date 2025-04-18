3d9b6b2c8b4205ce8286c650b4d95b6a5b8940a7
Nisim Levi <nisimlevi@gmail.com>
2025-04-18 03:19:11 +0300

    commit #6 - vite.config.js fixed

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,29 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // Custom plugin to handle JSX MIME types
+    {
+      name: 'configure-jsx-mime-type',
+      configureServer(server) {
+        server.middlewares.use((req, res, next) => {
+          if (req.url.endsWith('.jsx')) {
+            res.setHeader('Content-Type', 'application/javascript');
+          }
+          next();
+        });
+      },
+      configurePreviewServer(server) {
+        server.middlewares.use((req, res, next) => {
+          if (req.url.endsWith('.jsx')) {
+            res.setHeader('Content-Type', 'application/javascript');
+          }
+          next();
+        });
+      }
+    }
+  ],
   base: '/', // Base path for GitHub Pages deployment
   server: {
     fs: {
